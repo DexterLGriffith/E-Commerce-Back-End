2a3fa226d4d28bd7e3068cd1da612d929aa1720e
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-11-27 13:48:27 -0600

    finished product tag and tag.js models.

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
@@ -6,6 +6,22 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: { 
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true,
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {model: "product", key: "id",},
+    },
+    tag_id: { 
+      type: DataTypes.INTEGER, 
+      references: {model: "Tag", key: "id",},
+    },
+
     // define columns
   },
   {
